5c5b2d40f6eec98abafac2cc1acf1195e5df64ed
Bill <a0955361101@gmail.com>
2023-06-28 17:20:13 +0800

    note

--- Main.java
+++ Main.java
@@ -23,6 +23,72 @@ public class Main {
         // 聲明變數的方法
         String variableName = "value";
         System.out.println(variableName);
+
+        String name = "shadow";
+        System.out.println(name);
+
+        int myNum = 15;
+        System.out.println(myNum);
+
+        // 可以先聲明變數,再賦值
+        int myNumber;
+        myNumber = 20;
+        System.out.println(myNumber);
+
+        // 使用關鍵字 final 意味著不可更改且只讀 
+        final String myName = "B";
+        System.out.println(myName);
+
+        // 變量組合
+        String newName = name + myName;
+        System.out.println(newName);
+
+        int x = 5;
+        int y = 6;
+        System.out.println(x + y);
+
+        // 相同類型聲明多個變數
+        int a = 5,  b = 6,  c = 50;
+        System.out.println(a + b + c);
+
+        // 一個值對應多個變數
+        int d, e, f;
+        d = e = f = 50;
+        System.out.println(d + e + f);
+
+        // 數字 
+        // 整數類型
+        // byte數據類型可以存儲從 -128 到 127 的整數。int當您確定該值在 -128 到 127 之間時，可以使用它代替或其他整數類型來節省內存
+        byte myByte = 100;
+        System.out.println(myByte);
+
+        // short數據類型可以存儲從-32768到32767的整數
+        short myShort = 5000;
+        System.out.println(myShort);
+
+        // int數據類型可以存儲從 -2147483648 到 2147483647 的整數。一般來說，在我們的教程中，int當我們創建具有數值的變量時，該數據類型是首選數據類型。
+        int myInt = 100000;
+        System.out.println(myInt);
+
+        // long數據類型可以存儲從 -9223372036854775808 到 9223372036854775807 的整數。當 int 不足以存儲該值時使用此類型。請注意，您應該以“L”結束該值
+        long myLong = 15000000000L;
+        System.out.println(myLong);
+
+        // 浮點類型
+        // float類型double可以存儲小數。請注意，對於浮點型，應以“f”結尾，對於雙精度型，應以“d”結尾
+        float myFload = 5.75f;
+        System.out.println(myFload);
+        double myDouble = 19.99d;
+        System.out.println(myDouble);
+        // 使用float或double？
+        // 浮點值的精度表示該值小數點後可以有多少位。的精度float只有六位或七位小數，而double變量的精度約為15位。因此，對於大多數計算來說使用它更安全double
+
+        // 科學數字
+        // 浮點數也可以是科學數，用“e”表示 10 的冪
+        float f1 = 35e3f;
+        double d1 = 12E4d;
+        System.out.println(f1);
+        System.out.println(d1);
     }
 }
 
--- README.md
+++ README.md
@@ -1,9 +1,33 @@
-安裝java [oracle](https://www.oracle.com/java/technologies/java-se-glance.html)
+## 安裝java [oracle](https://www.oracle.com/java/technologies/java-se-glance.html)
 
-windows 設置 :
+### windows 設置 :
 
 1. windows 鍵 + R 輸入 sysdm.cpl
 2. 選進階 -> 環境變數 -> 在系統變數那 點擊新增
 3. 變數名稱 JAVA_HOME 變數值為安裝的路徑,未更改的情況下應該會是C:\Program Files\Java\jdk-所安裝的 jdk 版本
 4. 接下來編輯系統變數裡面的 Path 在 Path 最下面新增 %JAVA_HOME%\bin
 5. 打開 cmd 輸入 ava -version 如果順利顯示 "javac 版本號",表示 JDK 已正確的設定完成
+
+
+### 命名變量的一般規則是：
+
+1. 名稱可以包含字母、數字、下劃線和美元符號
+2. 名稱必須以字母開頭
+3. 名稱應以小寫字母開頭，且不能包含空格
+4. 名稱也可以以 $ 和 _ 開頭
+5. 名稱區分大小寫（“myVar”和“myvar”是不同的變數）
+保留字（如 Java 關鍵字，如int或 boolean）不能用作名稱
+
+### 原始數據類型 :
+原始數據類型指定變量值的大小和類型，並且沒有附加方法。
+
+| Data Type | Size | Description |
+| --------- | ---- | ----------- |
+| byte | 1byte | Stores whole numbers from -128 to 127 |
+| short | 2bytes | Stores whole numbers from -32,768 to 32,767 |
+| int | 4bytes | Stores whole numbers from -2,147,483,648 to 2,147,483,647 |
+| long | 8bytes | Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807 |
+| float | 4bytes | Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits |
+| double | 8bytes | Stores fractional numbers. Sufficient for storing 15 decimal digits |
+| boolean | 1bit | Stores true or false values |
+| char | 2bytes | Stores a single character/letter or ASCII values |
